516b2825d0922e74a13c74f6d6586ab77d3725ea
fanzhaofang <zhaofang.fan@hand-china.com>
2024-08-12 23:31:41 +0800

    [sda] 2

--- src/main/java/com/example/sync/Main2.java
+++ src/main/java/com/example/sync/Main2.java
@@ -10,6 +10,7 @@ import java.util.Scanner;
 public class Main2 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+
         // 注意 hasNext 和 hasNextLine 的区别
         while (in.hasNextLine()) { // 注意 while 处理多个 case
             String a = in.nextLine();
